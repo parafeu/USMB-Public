d10480ecf8a31fe3061060ec378301653aeb4528
Alben <benoit.alexandre.vision@gmail.com>
2017-09-07 13:35:49 +0200

    Exercice2 done

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -55,3 +55,52 @@ message='The average of positive samples of {list_value} is {res}'.format(list_v
                                                                           res=result)
 print(message)
 """
+
+def max_value(input_list):
+    ##
+    # basic function able to return the max value of a list
+    # @param input_list : the input list to be scanned
+    # @throws an exception (ValueError) on an empty list
+    
+    #first check if provided list is not empty
+    if len(input_list)==0:
+        raise ValueError('provided list is empty')
+    #init max_value and its index
+    max_val=input_list[0]
+    max_idx=0
+    #compute the average of positive elements of a list
+    """for item in input_list:
+        #select only positive items
+        if max_val<item:
+            max_val=item
+    """
+    #generic style : iterate over the range of list indexes 
+    for idx in range(len(input_list)):
+        #select only positive items
+        if max_val<input_list[idx]:
+            max_val=input_list[idx]
+            max_idx=idx
+            
+    
+    #generic style : iterate over the range of list indexes 
+    for idx, item in enumerate(input_list):
+        #select only positive items
+        if max_val<item:
+            max_val=item
+            max_idx=idx
+            
+            
+    return max_val, max_idx
+"""
+#test max_value function
+#1 basic test, expected answer=2
+mylist=[-1,2,-20]
+mymax, mymaxidx=max_value(mylist)
+mymax_tuple=max_value(mylist)
+mymax=mymax_tuple[0]
+print('Max value of {input_list} is {max_scan}'.format(input_list=mylist, max_scan=mymax))
+#==> message : "Max value of [-1, 2, -20] is (2, 1)"
+
+#2 error test : Exception expected 
+max_value([])
+"""
