786f19ddc4ced32ecdede3d6de68402734743d74
Alben <benoit.alexandre.vision@gmail.com>
2017-09-10 11:31:07 +0200

    added some comments, functions keep issues to be shown by unit tests

--- assignments/Session1/S1_algotools.py
+++ assignments/Session1/S1_algotools.py
@@ -26,13 +26,17 @@ mylist_sum=mylist+mylist2
 
 
 def average_above_zero(input_list):
-    
+    ##
+    # compute the average of positive values
+    # @input_list : the list of values to process
+    # @return the average value of all the positive elements
+
     #init critical variable
     positive_values_sum=0
     positive_values_count=0
-    
-    first_item=input_list[0]
-    
+
+    first_item=input_list[0] #just a line to generate a code smell with an unused value
+
     #compute the average of positive elements of a list
     for item in input_list:
         #select only positive items
@@ -61,7 +65,7 @@ def max_value(input_list):
     # basic function able to return the max value of a list
     # @param input_list : the input list to be scanned
     # @throws an exception (ValueError) on an empty list
-    
+
     #first check if provided list is not empty
     if len(input_list)==0:
         raise ValueError('provided list is empty')
@@ -74,22 +78,21 @@ def max_value(input_list):
         if max_val<item:
             max_val=item
     """
-    #generic style : iterate over the range of list indexes 
+    #generic style : iterate over the range of list indexes
     for idx in range(len(input_list)):
         #select only positive items
         if max_val<input_list[idx]:
             max_val=input_list[idx]
             max_idx=idx
-            
-    
-    #generic style : iterate over the range of list indexes 
+
+
+    #generic style : iterate over the range of list indexes
     for idx, item in enumerate(input_list):
         #select only positive items
         if max_val<item:
             max_val=item
             max_idx=idx
-            
-            
+
     return max_val, max_idx
 """
 #test max_value function
@@ -101,6 +104,55 @@ mymax=mymax_tuple[0]
 print('Max value of {input_list} is {max_scan}'.format(input_list=mylist, max_scan=mymax))
 #==> message : "Max value of [-1, 2, -20] is (2, 1)"
 
-#2 error test : Exception expected 
+#2 error test : Exception expected
 max_value([])
+"""
+
+"""
+# hints to solve the roi_bbox function exercise: numpy basics
+
+#matrix processing lib
+import numpy
+
+#create a numpy matrix with specific dimensions
+size_rows=10
+size_cols=10
+myMat=numpy.zeros([size_rows, size_cols], dtype=int)
+#set a value in a specific cell
+myMat[1,3]=1
+
+#fil something in the matrix, the basic way (a very slow python way...)
+for row in range(5,8):
+    for col in range(7,9):
+        myMat[row,col]=1
+
+#get time to measure processing speed
+import time
+init_time=time.time()
+
+#filling something in the matrix, a nicer way
+myMat[2:4,5:9]=1 #assign a scalar to each cell of a subarray
+myMat[4:7,7:9]=numpy.ones([3,2]) #copy an array in a subarray
+print(myMat)
+
+#get ellapsed time
+filling_time=time.time() -init_time
+print('data prefecting time='+str(filling_time))
+
+#fake bounding box coordinates matrix
+xmin=0
+xmax=100
+ymin=0
+ymax=200
+#how to fill the 4x2 bbox coordinates matrix, method 1 using 1D numpy arrays (ugly?)
+bbox_coords=numpy.zeros([4, 2], dtype=int)
+bbox_coords[0,:]=numpy.array([ymin, xmin])
+bbox_coords[1,:]=numpy.array([ymin, xmax])
+bbox_coords[2,:]=numpy.array([ymax, xmin])
+bbox_coords[3,:]=numpy.array([ymax, xmax])
+#how to fill the 4x2 bbox coordinates matrix, method 2 using lists (shorter way)
+#->create a list of lists
+coordsList=[[ymin, xmin],[ymin, xmax],[ymax, xmin],[ymax, xmax]]
+#->convert to an array
+coords_array=numpy.array(coordsList)
 """
